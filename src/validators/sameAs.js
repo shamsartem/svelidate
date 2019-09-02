e7d7d1905f701bd5523215392fe1a890eb64f162
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2019-09-02 11:06:54 +0300

    Fixed sameAs validator

--- src/validators/sameAs.js
+++ src/validators/sameAs.js
@@ -3,6 +3,6 @@ export default (equalTo) => function sameAs (value, outerF, f) {
   if (typeof equalTo === 'function') {
     return [value === equalTo(outerF, f).$val, [equalTo(outerF, f)]]
   } else {
-    return [value === f[equalTo], [f[equalTo]]]
+    return [value === f[equalTo].$val, [f[equalTo]]]
   }
 }
